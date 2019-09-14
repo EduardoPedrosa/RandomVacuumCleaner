da79afee4ecaef12288472d6ea92067ad08dbc5a
João Pedro <pedro.fachini.99@gmail.com>
2019-09-14 12:32:27 -0300

    comeco implementacao logica

--- src/cleaningRobot.js
+++ src/cleaningRobot.js
@@ -3,10 +3,15 @@
 class World {
     constructor(numFloors) {
         this.location = 0;
-        this.floors = [];
+        this.floors = [];        
         for (let i = 0; i < numFloors; i++) {
             this.floors.push({dirty: false});
         }
+        
+        //weight of each position
+        this.weight = [0, 0, 0,
+                       0, 0, 0,
+                       0, 0, 0]
     }
 
     markFloorDirty(floorNumber) {
@@ -42,8 +47,35 @@ class World {
 
         return action;
     }
-}
 
+    simulateGoalBased(action){
+        switch(action){
+            case 'SUCK':
+                this.floors[this.location].dirty = false
+                break
+            case 'LEFT':
+                if((this.location % 3 === 1) || (this.location % 3 === 2)){
+                    this.location -= 1
+                }
+                break
+            case 'RIGHT':
+                if((this.location % 3 === 0) || (this.location % 3 === 1)){
+                    this.location += 1
+                }
+                break
+            case 'UP':
+                if(this.location > 2){
+                    this.location -= 3
+                }
+                break
+            case 'DOWN':
+                if(this.location < 6){
+                    this.location +=3
+                }
+                break
+        }
+    }
+}
 
 // Rules are defined in code
 function reflexVacuumAgent(world) {
@@ -76,6 +108,24 @@ function reflexVacuumAgentKnowing(world) {
     }
 }
 
+function goalBasedVacuumAgent(world){
+    const rand = Math.random()
+    const possibleCases = ['LEFT', 'UP', 'RIGHT', 'DOWN']
+    if(world.floors[world.location].dirty) { return 'SUCK'; }
+    else if(world.location % 3 === 0){
+        possibleCases.shift() //doesn't go left
+    }
+    else if(world.location % 3 === 2){
+        possibleCases.splice(2, 1) //doesn't go right
+    }
+    else if(world.location < 3){
+        possibleCases.splice(1,1) //doesn't go up
+    }
+    else if(world.location > 5){
+        possibleCases.pop() //doesn't go down
+    }
+
+}
 // Rules are defined in data, in a table indexed by [location][dirty]
 function tableVacuumAgent(world, table) {
     let location = world.location;
